15bf8f76345a0bdf3e49e0ee19356db33f9004ac
wrjodjana <141975732+wrjodjana@users.noreply.github.com>
2023-12-07 15:06:41 -0600

    completed game 10

--- controllers/game10.js
+++ controllers/game10.js
@@ -1,8 +1,6 @@
-const Book = require("../models/game10book");
-const SortingGame = require("../models/game10sorting");
-const ChoiceGame = require("../models/game10choice");
-const MatchGame = require("../models/game10match");
-const { S3Client, PutObjectCommand, GetObjectCommand } = require("@aws-sdk/client-s3");
+const game10Asset = require("../model/game8Asset");
+const game10Audio = require("../model/game8Audio");
+const { S3Client, GetObjectCommand, PutObjectCommand, DeleteObjectCommand } = require("@aws-sdk/client-s3");
 const dotenv = require("dotenv");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 
@@ -73,6 +71,7 @@ const addAsset = async (req, res) => {
 
   // storing data in mongodb
   var Asset;
+  var flag = false;
   await game10Asset.findOne({ name: req.body.name }).then(async (asset) => {
     if (asset) {
       flag = true;
@@ -101,6 +100,10 @@ const addAsset = async (req, res) => {
         });
     }
   });
+
+  if (flag) {
+    return;
+  }
 };
 
 const addAudio = async (req, res) => {
@@ -137,13 +140,24 @@ const addAudio = async (req, res) => {
   command = new PutObjectCommand(params);
   await s3.send(command);
 
-  var storyAudio1 = req.files["storyAudio3"][0];
+  var storyAudio1 = req.files["storyAudio4"][0];
 
   var params = {
     Bucket: bucketName,
-    Key: storyAudio3.originalname,
-    Body: storyAudio3.buffer,
-    ContentType: storyAudio3.mimetype,
+    Key: storyAudio4.originalname,
+    Body: storyAudio4.buffer,
+    ContentType: storyAudio4.mimetype,
+  };
+  command = new PutObjectCommand(params);
+  await s3.send(command);
+
+  var promptAudio = req.files["promptAudio"][0];
+
+  var params = {
+    Bucket: bucketName,
+    Key: promptAudio.originalname,
+    Body: promptAudio.buffer,
+    ContentType: promptAudio.mimetype,
   };
   command = new PutObjectCommand(params);
   await s3.send(command);
@@ -158,6 +172,7 @@ const addAudio = async (req, res) => {
           storyAudio2: storyAudio2.originalname,
           storyAudio3: storyAudio3.originalname,
           storyAudio4: storyAudio4.originalname,
+          promptAudio: promptAudio.originalname,
         });
         await document
           .save()
@@ -175,3 +190,156 @@ const addAudio = async (req, res) => {
       res.status(400).send({ error: error.toString() });
     });
 };
+
+const getAsset = async (req, res) => {
+  var flag = false;
+
+  await game10Asset
+    .findOne({ name: req.body.name })
+    .then(async (result) => {
+      if (result) {
+        var getObjectParams = {
+          Bucket: bucketName,
+          Key: result.storyImage1,
+        };
+        command = new GetObjectCommand(getObjectParams);
+        var storyImage1Url = await getSignedUrl(s3, command, { expiresIn: 3600 });
+
+        var getObjectParams = {
+          Bucket: bucketName,
+          Key: result.storyImage2,
+        };
+        command = new GetObjectCommand(getObjectParams);
+        var storyImage2Url = await getSignedUrl(s3, command, { expiresIn: 3600 });
+
+        var getObjectParams = {
+          Bucket: bucketName,
+          Key: result.storyImage3,
+        };
+        command = new GetObjectCommand(getObjectParams);
+        var storyImage3Url = await getSignedUrl(s3, command, { expresIn: 3600 });
+
+        var getObjectParams = {
+          Bucket: bucketName,
+          Key: result.storyImage4,
+        };
+        command = new GetObjectCommand(getObjectParams);
+        var storyImage4Url = await getSignedUrl(s3, command, { expiresIn: 3600 });
+
+        if (flag == false) {
+          res.status(200).send({
+            storyImage1: storyImage1Url,
+            storyImage2: storyImage2Url,
+            storyImage3: storyImage3Url,
+            storyImage4: storyImage4Url,
+          });
+        }
+      } else {
+        res.status(400).send("No scenario in the database");
+      }
+    })
+    .catch((error) => {
+      res.status(400).send({ error: error.toString() });
+    });
+};
+
+const getAudio = async (req, res) => {
+  var flag = false;
+  await game10Audio
+    .findOne({ name: req.body.name })
+    .then(async (result) => {
+      if (result) {
+        var getObjectParams = {
+          Bucket: bucketName,
+          Key: result.storyAudio1,
+        };
+        command = new GetObjectCommand(getObjectParams);
+        var storyAudio1Url = await getSignedUrl(s3, command, { expiresIn: 3600 });
+
+        var getObjectParams = {
+          Bucket: bucketName,
+          Key: result.storyAudio2,
+        };
+        command = new GetObjectCommand(getObjectParams);
+        var storyAudio2Url = await getSignedUrl(s3, command, { expressIn: 3600 });
+
+        var getObjectParams = {
+          Bucket: bucketName,
+          Key: result.storyAudio3,
+        };
+        command = new GetObjectCommand(getObjectParams);
+        var storyAudio3Url = await getSignedUrl(s3, command, { expressIn: 3600 });
+
+        var getObjectParams = {
+          Bucket: bucketName,
+          Key: result.storyAudio4,
+        };
+        command = new GetObjectCommand(getObjectParams);
+        var storyAudio4Url = await getSignedUrl(s3, command, { expressIn: 3600 });
+
+        var getObjectParams = {
+          Bucket: bucketName,
+          Key: result.promptAudio,
+        };
+        command = new GetObjectCommand(getObjectParams);
+        var promptAudioUrl = await getSignedUrl(s3, command, { expressIn: 3600 });
+
+        if (flag == false) {
+          res.status(200).send({
+            storyAudio1: storyAudio1Url,
+            storyAudio2: storyAudio2Url,
+            storyAudio3: storyAudio3Url,
+            storyAudio4: storyAudio4Url,
+            promptAudio: promptAudioUrl,
+          });
+        }
+      } else {
+        res.status(400).send("No scenario in the database");
+      }
+    })
+    .catch((error) => {
+      res.status(400).send({ error: error.toString() });
+    });
+};
+
+const getAllAssets = async (req, res) => {
+  try {
+    const assets = await game8Asset.find();
+    res.status(200).send(assets);
+  } catch (error) {
+    res.status(400).send({ error: error.toString() });
+  }
+};
+
+const getAllAudios = async (req, res) => {
+  try {
+    const audios = await game8Audio.find();
+    res.status(200).send(audios);
+  } catch (error) {
+    res.status(400).send({ error: error.toString() });
+  }
+};
+
+const deleteAsset = async (req, res) => {
+  try {
+    await game8Asset.deleteOne({ _id: req.params.id });
+    res.status(200).json({
+      message: "done",
+    });
+  } catch (error) {
+    res.send(error);
+  }
+};
+
+const deleteAudio = async (req, res) => {
+  try {
+    await game8Audio.deleteOne({ _id: req.params.id });
+    res.status(200).json({
+      message: "done",
+    });
+  } catch (error) {
+    res.send(error);
+  }
+};
+
+module.exports = { getAudio, getAsset, getAllAssets, getAllAudios, deleteAsset, deleteAudio };
--- routes/game10.js
+++ routes/game10.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const multer = require("multer");
+const { addAsset, addAudio } = require("../controllers/game10");
 
 const router = express.Router();
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
+
+router.route("/addAsset").post(upload.fields([{ name: "storyImage1" }, { name: "storyImage2" }, { name: "storyImage3" }, { name: "storyImage4" }]));
+router.route("/addAudio").post(upload.fields([{ name: "storyAudio1" }, { name: "storyAudio2" }, { name: "storyAudio3" }, { name: "storyAudio4" }, { name: "promptAudio" }]));
+
+router.route("/getAsset").get(getAsset);
+router.route("/getAudio").get(getAudio);
+
+router.route("/getAssets").get(getAllAssets);
+router.route("/getAudios").get(getAllAudios);
+
+router.route("/deleteAsset/:id").delete(deleteAsset);
+router.route("/deleteAudio/:id").delete(deleteAudio);
+
+module.exports = router;
