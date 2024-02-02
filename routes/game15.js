a0cec5903c517ac1303c5c57dae0eddece2d07f4
wrjodjana <141975732+wrjodjana@users.noreply.github.com>
2024-02-02 11:20:38 -0600

    Update game15.js

--- routes/game15.js
+++ routes/game15.js
@@ -1,13 +1,6 @@
 const express = require("express");
 const multer = require("multer");
-const {
-  addAsset,
-  getAsset,
-  getAllAssets,
-  getAssetById,
-  deleteById,
-  updateAsset,
-} = require("../controllers/game15");
+const { addAsset, getAsset, getAllAssets, getAssetById, deleteById, updateAsset } = require("../controllers/game15");
 const { authenticateToken, authorizeUser } = require("../auth/auth");
 const router = express.Router();
 const storage = multer.memoryStorage();
@@ -15,47 +8,17 @@ const upload = multer({ storage: storage });
 
 router
   .route("/addAsset")
-  .post(
-    upload.fields([
-      { name: "initialAudioPrompt" },
-      { name: "finalAudioPrompt" },
-      { name: "image" },
-      { name: "correctOptionAudio" },
-      { name: "option1Audio" },
-      { name: "option2Audio" },
-    ]),
-    authenticateToken,
-    authorizeUser,
-    addAsset
-  );
+  .post(upload.fields([{ name: "initialAudioPrompt" }, { name: "finalAudioPrompt" }, { name: "image" }, { name: "correctOptionAudio" }, { name: "option1Audio" }, { name: "option2Audio" }]), authenticateToken, authorizeUser, addAsset);
 
 router.route("/getAsset").get(getAsset);
 
-router
-  .route("/getAllAssets")
-  .get(authenticateToken, authorizeUser, getAllAssets);
+router.route("/getAllAssets").get(authenticateToken, authorizeUser, getAllAssets);
 
-router
-  .route("/getAsset/:id")
-  .get(authenticateToken, authorizeUser, getAssetById);
+router.route("/getAsset/:id").get(authenticateToken, authorizeUser, getAssetById);
 
-router
-  .route("/delete/:id")
-  .delete(authenticateToken, authorizeUser, deleteById);
+router.route("/delete/:id").delete(authenticateToken, authorizeUser, deleteById);
 
 router
   .route("/updateAsset/:id")
-  .put(
-    upload.fields([
-      { name: "initialAudioPrompt" },
-      { name: "finalAudioPrompt" },
-      { name: "image" },
-      { name: "correctOptionAudio" },
-      { name: "option1Audio" },
-      { name: "option2Audio" },
-    ]),
-    authenticateToken,
-    authorizeUser,
-    updateAsset
-  );
+  .put(upload.fields([{ name: "initialAudioPrompt" }, { name: "finalAudioPrompt" }, { name: "image" }, { name: "correctOptionAudio" }, { name: "option1Audio" }, { name: "option2Audio" }]), authenticateToken, authorizeUser, updateAsset);
 module.exports = router;
