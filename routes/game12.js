37c98508641026ecb9fee23a0d83dda2d93c9049
wrjodjana <warrjodjana@gmail.com>
2024-02-02 11:20:49 -0600

    Update game12.js

--- routes/game12.js
+++ routes/game12.js
@@ -1,16 +1,60 @@
 const express = require("express");
 const multer = require("multer");
-const { addAsset, addAudio, getAssetAndAudio, getAllAssets, getAllAudios, deleteAsset, deleteAudio, updateAsset, updateAudio } = require("../controllers/game12");
+const {
+  addAsset,
+  addAudio,
+  getAssetAndAudio,
+  getAllAssets,
+  getAllAudios,
+  deleteAsset,
+  deleteAudio,
+  updateAsset,
+  updateAudio,
+} = require("../controllers/game12");
 const router = express.Router();
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-router.route("/addAsset").post(upload.fields([{ name: "backgroundImage" }, { name: "wordImage" }, { name: "gif" }]), addAsset);
-router.route("/addAudio").post(upload.fields([{ name: "initialPromptAudio" }, { name: "lastPromptAudio" }]), addAudio);
+router
+  .route("/addAsset")
+  .post(
+    upload.fields([
+      { name: "backgroundImage" },
+      { name: "wordImage" },
+      { name: "gif" },
+    ]),
+    addAsset
+  );
+router
+  .route("/addAudio")
+  .post(
+    upload.fields([
+      { name: "initialPromptAudio" },
+      { name: "lastPromptAudio" },
+    ]),
+    addAudio
+  );
 
-router.route("/updateAsset/:id").put(upload.fields([{ name: "backgroundImage" }, { name: "wordImage" }, { name: "gif" }]), updateAsset);
-router.route("/updateAudio/:id").put(upload.fields([{ name: "initialPromptAudio" }, { name: "lastPromptAudio" }]), updateAudio);
+router
+  .route("/updateAsset/:id")
+  .put(
+    upload.fields([
+      { name: "backgroundImage" },
+      { name: "wordImage" },
+      { name: "gif" },
+    ]),
+    updateAsset
+  );
+router
+  .route("/updateAudio/:id")
+  .put(
+    upload.fields([
+      { name: "initialPromptAudio" },
+      { name: "lastPromptAudio" },
+    ]),
+    updateAudio
+  );
 
 router.route("/getAsset").get(getAssetAndAudio);
 
